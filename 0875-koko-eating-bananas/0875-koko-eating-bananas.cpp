900d44ab21056aa0b690c9ca0f848fc460958ee2
Anshul Singh Jadon <168765000+ANSHUL5911@users.noreply.github.com>
2026-06-27 18:22:45 +0530

    Time: 14 ms (20.5%), Space: 23 MB (46.85%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long findHours(vector<int>& piles, int n, int speed) {
+        long long hr = 0;
+        for (int i = 0; i < n; i++) {
+            hr = hr + piles[i] / speed;
+            if (piles[i] % speed != 0) {
+                hr++;
+            }
+        }
+        return hr;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        sort(piles.begin(), piles.end());
+
+        int low = 1, high = piles[n - 1];
+        int res = -1;
+
+        while (low <= high) {
+            int guess = (low + high) / 2;
+            long long hour = findHours(piles, n, guess);
+
+            if (hour > h) {
+                low = guess + 1;
+            } else {
+                res = guess;
+                high = guess - 1;
+            }
+        }
+        return res;
+    }
+};
